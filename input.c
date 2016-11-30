f696e0365eec1ae0fc5769962cb633e40663b46d
mpierdominici <mpierdominici@itba.edu.ar>
2016-11-30 11:21:21 -0300

    lectura de mouse en allegro, iniciando con el proyecto

---
+++ input.c
@@ -0,0 +1,12 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <allegro5/allegro.h>
+
+static ALLEGRO_DISPLAY *pantalla =NULL;
+static ALLEGRO_EVENT_QUEUE *cola_de_eventos =NULL;
