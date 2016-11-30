9b8387e5867323c547612d0d6d49b805fc799cf2
mmaspero <mmaspero@itba.edu.ar>
2016-11-30 00:12:48 -0300

    Rename print.h to mprint.h

---
+++ mprint.h
@@ -0,0 +1,21 @@
+#ifndef print_h
+#define print_h
+
+#include <stdio.h>
+#include "print.h"
+#include <stdint.h>
+
+#define GPIO_OPEN1 ("/sys/class/gpio/export")
+
+#define GPIO_VALUE1(n) ("/sys/class/gpio/gpio" #n "/value")
+#define GPIO_VALUE2(n) GPIO_VALUE1(n)
+
+#define GPIO_DIRECTION1(n) ("/sys/class/gpio/gpio" #n "/direction")
+#define GPIO_DIRECTION2(n) GPIO_DIRECTION1(n)
+
+void toggle ( uint8_t led);
+void *  thread (void *);
+void initiate_out (void);
+
+
+#endif /* print_h */
