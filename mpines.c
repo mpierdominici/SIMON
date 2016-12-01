5e3dc7ca4cb8a5130beb1ceb6c56a98566f94ba4
mmaspero <mmaspero@itba.edu.ar>
2016-12-01 15:21:46 -0300

    Update mpines.c

--- mpines.c
+++ mpines.c
@@ -38,3 +38,50 @@
         
         return 0;
     }
+
+
+
+/*
+ESTO ES PARA INPUT, PARECIDO A OUTPUT
+
+#include <stdio.h>
+
+#include "print.h"
+#include <pthread.h>
+#include "pin.h"
+
+
+int main(void)
+{
+    int i;
+    pthread_t ledoutput;
+    
+    MPIN pinesin []=
+    {
+        
+        {"4","/sys/class/gpio/gpio4/direction","/sys/class/gpio/gpio4/value"},
+        {"17",DIRECTION_P2(17),VALUE_P2(17)},
+        {"18",DIRECTION_P2(18),VALUE_P2(18)},
+        {"27",DIRECTION_P2(27),VALUE_P2(27)}
+        
+        
+    };
+    
+    set_gpio(pinesin);
+    
+    for (i=0;i<4;i++)
+    {
+        export_gpio(i);
+        direction_gpio(M_INPUT ,i);
+    }
+    
+    // no se donde guardar los valores pero aca se llamaria a la funcion que lo lea
+    // se llama read_value DEVULEVE: CHAR *
+    
+    
+    for (i=0;i<4;i++)
+        unexport_gpio(i);
+    
+    return 0;
+}
+*/
