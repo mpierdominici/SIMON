135c86585647b1dea94f7d34e824ca32b4af1cc2
mmaspero <mmaspero@itba.edu.ar>
2016-12-01 13:01:15 -0300

    Update mprint.c

--- mprint.c
+++ mprint.c
@@ -1,114 +1,168 @@
+
 #include "print.h"
-#include "boole.h"
 
 
-static char * leds[LED_NUM] = {"22","23","24","25"};
-static uint8_t ledstate [LED_NUM]={0};
+static MPIN * m_pines =NULL;
 
-void toggle ( uint8_t led)
-
+void set_gpio(MPIN * gpio_pines)
 {
-   if (ledstate [led] == M_FALSE)
-   {
-       ledstate [led]= M_TRUE;
-   }
-   else
-   {
-       ledstate [led]= M_FALSE;
-   }
+    m_pines=gpio_pines;
 }
 
+int export_gpio(char npin)
+{
+    FILE * gpiof=NULL;
+    int error=0;
+    
+    
+    if((gpiof = fopen(EXPORT_P,"w"))==NULL)
+    {
+        printf("No se pudo abrir el archivo\n");
+        error=M_ERROR;
+    }
+    
+    error = fputs(m_pines[npin].gpio_num,gpiof);
+    
+    if(error== M_ERROR)
+    {
+        printf("no se pudo exportar el pin %s",m_pines[npin].gpio_num);
+        error=M_ERROR;
+    }
+    else
+    {
+        printf("se exporto correctamente el pin %s",m_pines[npin].gpio_num);
+    }
+    fclose(gpiof);
+    return(error);
+}
+
+
+int unexport_gpio(char npin)
+{
+    FILE * gpiof=NULL;
+    int error=0;
+    
+    
+    if((gpiof = fopen(UNEXPORT_P,"w"))==NULL)
+    {
+        printf("No se pudo abrir el archivo\n");
+        error=M_ERROR;
+    }
+    
+    error = fputs(m_pines[npin].gpio_num,gpiof);
+    
+    if(error== M_ERROR)
+    {
+        printf("no se pudo desexportar el pin %s",m_pines[npin].gpio_num);
+        error=M_ERROR;
+    }
+    else
+    {
+        printf("se desexporto correctamente el pin %s",m_pines[npin].gpio_num);
+    }
+    fclose(gpiof);
+    return(error);
+}
+
+int direction_gpio(char direction ,char npin)
+{
+    FILE * gpiof=NULL;
+    int error=0;
+    
+    
+    if((gpiof = fopen(m_pines[npin].mdirection,"w"))==NULL)
+    {
+        printf("No se pudo abrir el archivo\n");
+        error=M_ERROR;
+    }
+    if(direction==M_INPUT)
+    {
+        error = fputs("in",gpiof);
+    }
+    else if(direction==M_OUTPUT)
+    {
+        error = fputs("out",gpiof);
+    }
+    
+    
+    if(error== M_ERROR)
+    {
+        printf("no se pudo direccionar el pin %s",m_pines[npin].gpio_num);
+        error=M_ERROR;
+    }
+    else
+    {
+        //printf("se exporto correctamente el pin %s",m_pines[npin].gpio_num);
+    }
+    fclose(gpiof);
+    return(error);
+}
+
+int value_gpio(char value ,char npin)
+{
+    FILE * gpiof=NULL;
+    int error=0;
+    
+    
+    if((gpiof = fopen(m_pines[npin].mvalue,"w"))==NULL)
+    {
+        printf("No se pudo abrir el archivo\n");
+        error=M_ERROR;
+    }
+    if(value==M_HIGH)
+    {
+        error = fputs("1",gpiof);
+    }
+    else if(value==M_LOW)
+    {
+        error = fputs("0",gpiof);
+    }
+    
+    
+    if(error== M_ERROR)
+    {
+        printf("no se pudo ASIGNAR VALOR el pin %s",m_pines[npin].gpio_num);
+        error=M_ERROR;
+    }
+    else
+    {
+        //printf("se exporto correctamente el pin %s",m_pines[npin].gpio_num);
+    }
+    fclose(gpiof);
+    return(error);
+}
 
 
 
 void * thread ()
 {
     FILE * pin_value =NULL;
-    int notwritten2;
     int i;
     
     while (1)
     {
-        for (i=0; i < LED_NUM; i++)
-        {
-            if ((pin_value=fopen(GPIO_VALUE2(leds [i]), "w")))
+     
+        for (i=0; i < 4; i++)
             {
-             printf ("ERROR opening the file");
-            }
+                if (estado_botones[i]==M_FALSE)
+                {
+                    value_gpio(M_LOW,i);
+                }
+                else
+                {
+                    value_gpio(M_HIGH,i);
+                }
             
-            if (ledstate[i]==M_FALSE)
-            {
-                notwritten2 = fputs(LED_OFF, pin_value);
+                fclose(pin_value);
             }
-            else
-            {
-                notwritten2 = fputs(LED_ON, pin_value);
-            }
-        
-            fclose(pin_value);
-        }
+         
+     }
+     
+ }
 
-    }
-
-}
-    
-    
-    
-    
-void initiate_out ()
+void change_led_state (char state, char i)
 {
-    int i;
-    FILE * pin_export = NULL;
-    FILE * pin_direction = NULL;
-    int notwritten;
-    
-    for (i=0;i<LED_NUM ;i++)
-    {
-        if ((pin_export = fopen (GPIO_OPEN1, "w"))== NULL)
-        {
-            printf ("ERROR opening the file");
-        }
-        
-    
-        notwritten = fputs ( leds [i], pin_export);
-        if (notwritten== M_ERROR)
-        {
-            printf("ERROR exporting pin");
-            
-        }
-        
-        fclose(pin_export);
-        
-        if ((pin_direction = fopen(GPIO_DIRECTION2(leds [i]), "w"))== NULL)
-        {
-            printf("ERROR opening pin");
-        }
-        
-        notwritten = fputs("out", pin_direction);
-        if ( notwritten== M_ERROR)
-        {
-            printf("ERROR setting pin as output");
-        }
-        
-        fclose(pin_direction);
-        
-    }
+    estado_botones[i]= state;
 }
+     
 
-/* ejemplo de main
-
-#include "print.h"
-#include <pthread.h>
-
-int main (void)
-{
-    pthread_t print_led;
-    
-    initiate_out();
-    
-    pthread_create(&print_led, NULL, thread, NULL);
-    pthread_join(print_led, NULL);
-    return 0;
-    
-}
-*/
